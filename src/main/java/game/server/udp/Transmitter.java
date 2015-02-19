4836eabf372f4ef8d37ba9bc1afdf79973dfd69e
Oleksandr_Rudenko1 <alexander.rudenko@hotmial.com>
2015-02-19 18:06:25 +0200

    Transmitter memory optimization

--- src/main/java/game/server/udp/Transmitter.java
+++ src/main/java/game/server/udp/Transmitter.java
@@ -1,6 +1,7 @@
 package game.server.udp;
 
 import game.server.ServerHandler;
+import game.server.ServerTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,22 +22,39 @@ public class Transmitter extends ServerHandler {
 
     private DatagramChannel channel;
     
-    private Map<Long, Packet> packets = new HashMap<>();
+    private Map<Long, Packet> index = new HashMap<>();
     private Queue<Packet> queue = new ArrayBlockingQueue<>(1024);
+    private Packet[] packets = new Packet[2048];
+    private int lastIndex = -1;
+    private int success;
+    
+    private int GC_THRESHOLD = 1000;
+    private long threshold;
+    
     
     public Transmitter(DatagramChannel channel) {
         this.channel = channel;
+        threshold = ServerTime.mills() + GC_THRESHOLD;
     }
 
     @Override
     public void handle() throws IOException {
-        Collection<Packet> outList = packets.values();
-        outList.removeIf(Packet::hasTransmitted);
-        for (Packet packet : packets.values()) {
-            if (packet.isReady()) {
+        for (int i = 0; i < packets.length; i++) {
+            Packet packet = packets[i];
+            if (packet == null) {
+                continue;
+            }
+
+            if (packet.hasTransmitted()) {
+                index.remove(packet.id);
+                packets[i] = null; //drop from array for gc
+                success++;
+            } else if (packet.isReady()) {
                 transmit(packet);
             }
         }
+        
+        clean();
         merge();
     }
 
@@ -47,7 +65,7 @@ public class Transmitter extends ServerHandler {
     }
     
     public void ack(UUID sessionId, long packetId, int part) {
-        Packet packet = packets.get(packetId);
+        Packet packet = index.get(packetId);
         if (packet != null) {
             if (sessionId.equals(packet.getSessionId())) {
                 packet.ack(part);
@@ -62,7 +80,7 @@ public class Transmitter extends ServerHandler {
     }
 
     public int packetsSize() {
-        return packets.size();
+        return lastIndex + 1 - success;
     }
     
     private void transmit(Packet packet) {
@@ -87,9 +105,49 @@ public class Transmitter extends ServerHandler {
     }
 
     private void merge() {
+        if (queue.isEmpty()) {
+            return;
+        }
+
+        int size = queue.size();
+        if (lastIndex + size >= packets.length) {
+            clean();
+        }
+        
         Packet packet;
         while ((packet = queue.poll()) != null) {
-            packets.put(packet.id, packet);
+            if (lastIndex == packets.length - 1) {
+                //TODO: fix extension
+                break;
+            } else {
+                lastIndex++;
+                packets[lastIndex] = packet;
+                index.put(packet.id, packet);
+            }
+        }
+    }
+    
+    private void clean() {
+        if (success > GC_THRESHOLD || threshold > ServerTime.mills()) {
+            int k = 0;
+            for (int i = lastIndex; i >= k; i--) {
+                if (packets[i] != null) {
+                    for (int j = k; j < i; j++) {
+                        if (packets[j] == null) {
+                            packets[j] = packets[i];
+                            packets[i] = null;
+                            k = j;
+                            lastIndex--;
+                            break;
+                        }
+                    }
+                } else {
+                    lastIndex--;
+                }
+            }
+            
+            
+            threshold = ServerTime.mills() + GC_THRESHOLD;
         }
     }
 }
