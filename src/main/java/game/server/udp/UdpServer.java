6c69beb17737032d1927c41c0d8ba5a587d55a5b
Oleksandr Rudenko <orudenko@conductor.com>
2018-05-23 17:57:27 +0300

    Fix error on server shutdown

--- src/main/java/game/server/udp/UdpServer.java
+++ src/main/java/game/server/udp/UdpServer.java
@@ -1,17 +1,16 @@
 package game.server.udp;
 
+import game.server.ServerTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.SocketException;
 import java.nio.channels.DatagramChannel;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import game.server.ServerTime;
 
 public class UdpServer {
 
@@ -30,25 +29,25 @@ public class UdpServer {
     private SocketAddress address;
     private DatagramSocket socket;
 
-    public UdpServer(int port) throws IOException {
+    public UdpServer(int port) {
         ServerTime.lockAsProduction();
         address = new InetSocketAddress(port);
     }
 
     public void start() throws SocketException {
-        LOG.info("Server started");
         socket = channel.socket();
         socket.bind(address);
-        
+
         new Thread(group, receiver).start();
         new Thread(group, transmitter).start();
+        LOG.info("Server started on port {}", socket.getLocalPort());
     }
 
     public void stop() throws IOException {
+        channel.close();
         receiver.stop();
         transmitter.stop();
         socket.close();
-        channel.close();
         LOG.info("Server stopped");
     }
 
